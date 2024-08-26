d56a26796cee510632c87bcb00c19194bdd8d4dd
Joely Silva <joely@ubots.com.br>
2024-08-26 14:10:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const { Console } = require('console');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Digite algo: ', (resposta) => {
+    let Word = resposta.split(" ");
+    let AllWords = "";
+
+    for (let i = 0; i < Word.length; i++) {
+        TotalOfStrings = Word[i].length;
+        MiddleOfWord = Word[i].length / 2;
+        let parte1 = Word[i].substring(0, MiddleOfWord);
+        let parte2 = Word[i].substring((TotalOfStrings - MiddleOfWord));
+        let result = `**${parte1}**${parte2}`;
+        AllWords += result + ",";
+        
+    }
+    console.log(AllWords);
+    rl.close();
+});
