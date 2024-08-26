52e8b67e8b8aaa14be77490a6cd8e7c975a2360d
Joely Silva <joely@ubots.com.br>
2024-08-26 16:20:34 -0300

    teste adicionado e correção do código

--- app.js
+++ app.js
@@ -6,18 +6,35 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Digite algo: ', (resposta) => {
-    let Word = resposta.split(" ");
-    let AllWords = "";
-
-    for (let i = 0; i < Word.length; i++) {
-        TotalOfStrings = Word[i].length;
-        MiddleOfWord = Word[i].length / 2;
-        let parte1 = Word[i].substring(0, MiddleOfWord);
-        let parte2 = Word[i].substring((TotalOfStrings - MiddleOfWord));
-        let result = `**${parte1}**${parte2} `;
-        AllWords += result;
-    }
-    console.log(AllWords);
+rl.question('Digite algo: ', (input) => {
+    let result = ConcatStariskInTheWord(input);
+    console.log(result);
     rl.close();
 });
+
+function ConcatStariskInTheWord(input) {
+    let words = input.split(" ");
+    let allWords = "";
+    for (let i = 0; i < words.length; i++) {
+        let totalOfStrings = words[i].length;
+        let middleOfWord = words[i].length / 2;
+        let parte1 = words[i].substring(0, middleOfWord);
+        let parte2 = words[i].substring((totalOfStrings - middleOfWord));
+        let result = `**${parte1}**${parte2}`;
+        if (i < words.length) { result += ` `; }
+        allWords += result;
+    }
+    return allWords;
+}
+
+/*
+Quais testes que eu faria?
+- Provar que funciona
+- Testar para garantir que não da problema
+- Espaços
+- Limite de Caracteres
+- Ignorar palavras com duas letras apenas
+
+Ver os testes nativos do Node;
+Rodar todos os testes, com apenas um comando, como o NPM run build da plataforma, mais simples possível.
+*/
